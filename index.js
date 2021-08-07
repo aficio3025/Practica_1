666af0b8aedbaebdc1dd0cff76a9fe3fbb470bf2
Gustavo <adolfosarate@gmail.com>
2021-08-06 21:19:39 -0300

    branch suma de tres numeros terminado

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function sumar(a,b,c) {
     const d = a + b + c;
-    return d;
- }
+    console.log('el resultado de la suma es: ', d);
+    //return d;
+    
+}
 
- module.exports = { sumar };
+sumar(1, 5, 8);
+ 
+//module.exports = { sumar };
+
+//Para realizar el test habilitar las linea 4 y 10. Y comentar las lineas 3 y 8
